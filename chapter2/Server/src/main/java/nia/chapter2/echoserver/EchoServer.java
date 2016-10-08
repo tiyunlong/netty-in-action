7e560c0aa330e01b26734b05a09ee84b2310c42a
tiyunlong <tiyunlong@gmail.com>
2016-10-08 14:58:13 +0800

    test

--- chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
+++ chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
@@ -11,49 +11,46 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import java.net.InetSocketAddress;
 
 /**
- * Listing 2.2  of <i>Netty in Action</i>
+ * Listing 2.2 of <i>Netty in Action</i>
  *
  * @author <a href="mailto:norman.maurer@gmail.com">Norman Maurer</a>
  */
 public class EchoServer {
-    private final int port;
 
-    public EchoServer(int port) {
-        this.port = port;
-    }
+	private final int port;
 
-    public static void main(String[] args)
-        throws Exception {
-        if (args.length != 1) {
-            System.err.println("Usage: " + EchoServer.class.getSimpleName() +
-                " <port>"
-            );
-            return;
-        }
-        int port = Integer.parseInt(args[0]);
-        new EchoServer(port).start();
-    }
+	public EchoServer(int port) {
+		this.port = port;
+	}
 
-    public void start() throws Exception {
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            ServerBootstrap b = new ServerBootstrap();
-            b.group(group)
-                .channel(NioServerSocketChannel.class)
-                .localAddress(new InetSocketAddress(port))
-                .childHandler(new ChannelInitializer<SocketChannel>() {
-                    @Override
-                    public void initChannel(SocketChannel ch) throws Exception {
-                        ch.pipeline().addLast(new EchoServerHandler());
-                    }
-                });
+	public static void main(String[] args) throws Exception {
+		if (args.length != 1) {
+			System.err.println("Usage: " + EchoServer.class.getSimpleName() + " <port>");
+			return;
+		}
+		int port = Integer.parseInt(args[0]);
+		new EchoServer(port).start();
+	}
 
-            ChannelFuture f = b.bind().sync();
-            System.out.println(EchoServer.class.getName() +
-                " started and listening for connections on " + f.channel().localAddress());
-            f.channel().closeFuture().sync();
-        } finally {
-            group.shutdownGracefully().sync();
-        }
-    }
+	public void start() throws Exception {
+		EventLoopGroup group = new NioEventLoopGroup();
+		try {
+			ServerBootstrap b = new ServerBootstrap();
+			b.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port))
+			        .childHandler(new ChannelInitializer<SocketChannel>() {
+
+				        @Override
+				        public void initChannel(SocketChannel ch) throws Exception {
+					        ch.pipeline().addLast(new EchoServerHandler());
+				        }
+			        });
+
+			ChannelFuture f = b.bind().sync();
+			System.out.println(EchoServer.class.getName() + " started and listening for connections on "
+			        + f.channel().localAddress());
+			f.channel().closeFuture().sync();
+		} finally {
+			group.shutdownGracefully().sync();
+		}
+	}
 }
